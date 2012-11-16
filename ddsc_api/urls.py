ceb89bc8461460c77377041e06f5ed6ec1d6fc22
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-11-16 13:53:37 +0100

    Cleaned up urls.

--- ddsc_api/urls.py
+++ ddsc_api/urls.py
@@ -7,19 +7,15 @@ from lizard_ui.urls import debugmode_urlpatterns
 
 from ddsc_api import views
 
+
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
 #    url(r'^$', HomepageView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^ui/', include('lizard_ui.urls')),
+#    url(r'^ui/', include('lizard_ui.urls')),
     url(r'^api/', include('dikedata_api.urls')),
-    # url(r'^something/',
-    #     views.some_method,
-    #     name="name_it"),
-    # url(r'^something_else/$',
-    #     views.SomeClassBasedView.as_view(),
-    #     name='name_it_too'),
     )
+
 urlpatterns += debugmode_urlpatterns()
