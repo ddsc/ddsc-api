63c00fa1f300121e6db811a45677bb2490c560a0
Berto Booijink <berto.booijink@nelen-schuurmans.nl>
2012-11-28 15:16:06 +0100

    Added redirect from root url to /api. Also removed some clutter.

--- ddsc_api/urls.py
+++ ddsc_api/urls.py
@@ -3,18 +3,16 @@ from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 from lizard_ui.urls import debugmode_urlpatterns
-
-from ddsc_api import views
 
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-#    url(r'^$', HomepageView.as_view()),
+    url(r'^$', redirect_to, {'url': 'api'}),
     url(r'^admin/', include(admin.site.urls)),
-#    url(r'^ui/', include('lizard_ui.urls')),
     url(r'^api/', include('dikedata_api.urls')),
 )
 
