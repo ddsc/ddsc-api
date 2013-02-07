bcec7b6fdf7bbfd3f75b03534caae182f9372efe
Roland van Laar <roland@micite.net>
2013-02-07 12:13:17 +0100

    Added collageitems to root view.
    Use sortedDict for rootview to preserveorder.
    Easier for us as developers.

--- ddsc_api/views.py
+++ ddsc_api/views.py
@@ -2,6 +2,8 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
 
+from collections import OrderedDict
+
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,15 +11,16 @@ from rest_framework.response import Response
 
 class Root(APIView):
     def get(self, request, format=None):
-        response = {
-            'datasets': reverse('dataset-list', request=request),
-            'locations': reverse('location-list', request=request),
-            'timeseries': reverse('timeseries-list', request=request),
-            'parameters': reverse('parameter-list', request=request),
-            'logicalgroups': reverse('logicalgroup-list', request=request),
-            'layers': reverse('layer-list', request=request),
-            'collages': reverse('collage-list', request=request),
-        }
+        response = OrderedDict([
+            ('datasets', reverse('dataset-list', request=request)),
+            ('locations', reverse('location-list', request=request)),
+            ('timeseries', reverse('timeseries-list', request=request)),
+            ('parameters', reverse('parameter-list', request=request)),
+            ('logicalgroups', reverse('logicalgroup-list', request=request)),
+            ('layers', reverse('layer-list', request=request)),
+            ('collages', reverse('collage-list', request=request)),
+            ('collageitems', reverse('collageitem-list', request=request)),
+            ])
 
         user = getattr(request, 'user', None)
         if user is not None and user.is_superuser:
