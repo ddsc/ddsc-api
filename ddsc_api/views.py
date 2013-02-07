70349917cadbd5258cb44f3b3367cd51abd35b15
Roland van Laar <roland@micite.net>
2013-02-07 14:23:44 +0100

    Collage Item was removed from ddsc-site.

--- ddsc_api/views.py
+++ ddsc_api/views.py
@@ -19,7 +19,6 @@ class Root(APIView):
             ('logicalgroups', reverse('logicalgroup-list', request=request)),
             ('layers', reverse('layer-list', request=request)),
             ('collages', reverse('collage-list', request=request)),
-            ('collageitems', reverse('collageitem-list', request=request)),
             ])
 
         user = getattr(request, 'user', None)
