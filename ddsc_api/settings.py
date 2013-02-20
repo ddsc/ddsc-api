8d4e61ff2f1530659f74770dd703fdc1975becdb
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-02-20 10:23:48 +0100

    fix CORS as announced per email

--- ddsc_api/settings.py
+++ ddsc_api/settings.py
@@ -167,8 +167,14 @@ REST_FRAMEWORK = {
     'PAGINATE_BY_PARAM': 'page_size'
 }
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = [
+    '127.0.0.1',
+    '192.168.56.101',
+    'dijkdata.nl',
+    'test.dijkdata.nl',
+]
 
 # TODO: Put your real url here to configure Sentry.
 # override me in localsettings
