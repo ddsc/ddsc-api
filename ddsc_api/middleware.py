e3d88f3f40b3a194e575f051a8d64a2b3475ae89
ejnens <erikjan.vos@nelen-schuurmans.nl>
2013-06-03 11:12:26 +0200

    more aggressively disable caching to fix IE9

--- ddsc_api/middleware.py
+++ ddsc_api/middleware.py
@@ -3,8 +3,7 @@ from django.utils.cache import add_never_cache_headers
 class DisableClientSideCaching(object):
     def process_response(self, request, response):
         try:
-            if request.user.is_authenticated():
-                add_never_cache_headers(response)
+            add_never_cache_headers(response)
         except:
             pass
         return response
